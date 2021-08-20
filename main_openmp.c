e4486a3af3c3fda466478bf8a2d68eae3076081e
Vinicius Gasparini <viniciuszeiko@gmail.com>
2021-08-20 17:29:28 -0300

    feat: update with multiple read

--- main_openmp.c
+++ main_openmp.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
-#include <unistd.h>
 
-double v[10000007];
-double aux[10000007];
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
+double vetor[100000008];
 int n_threads;
+double maximo, minimo;
 
 void swap(double *a, double *b) {
     double t = *a;
@@ -30,8 +30,6 @@ int divide(double arr[], int low, int high) {
 void quicksort(double arr[], int low, int high) {
     if (low < high) {
         int pivot = divide(arr, low, high);
-// Referência: 6.2.1.1 Parallel Task
-// Paralelismo de tarefas utilizando OpenMP - Bianchini, Valoboas, Castro
 #pragma omp task final((pivot - low) < n_threads) mergeable default(none) \
     shared(arr) firstprivate(low, pivot)
         quicksort(arr, low, pivot - 1);
@@ -42,65 +40,70 @@ void quicksort(double arr[], int low, int high) {
     }
 }
 
-double wtime() {
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return t.tv_sec + t.tv_usec / 1000000.0;
-}
-
-int read_file(double *vetor, const char *filename) {
-    int index = 0;
+int open_file(int idx, char *files[]) {
+    maximo = 0xffffffff;
+    maximo = -0xffffffff;
     FILE *filep;
-    char filaname_in[1000];
-    strcpy(filaname_in, "entradas/");
-    strcat(filaname_in, filename);
-    if ((filep = fopen(filaname_in, "r")) == NULL) {
-        printf("Failed to open file!\n");
-        // return 1;
+    char filename[100];
+    strcpy(filename, "entradas/");
+    strcat(filename, files[idx]);
+    filep = fopen(filename, "r");
+    int index = 0;
+    if (filep == NULL) {
+        fprintf(stderr, "Cannot open file %s\n", filename);
+        exit(1);
     } else {
         while ((fscanf(filep, "%lf", &vetor[index++])) != EOF) {
         }
     }
+    fclose(filep);
     return index - 1;
 }
 
-int main(int const argc, char const *argv[]) {
-    const char *filename = argc > 1 ? argv[1] : "1000000.txt";
-    n_threads = (argc > 2) ? atoi(argv[2]) : 4;
-    omp_set_num_threads(n_threads);
-    int n = read_file(v, filename);
-
-    // Começando a medição do tempo
-    double start_time = omp_get_wtime();
-
+void solve(int idx, char *files[], int size) {
 #pragma omp parallel
 #pragma omp single
-    quicksort(v, 0, n - 1);
-    double ma = v[0], dp, mg = v[0], md, p95, minimo = v[0], maximo = v[n - 1];
-    for (int i = 1; i < n; i++) {
-        double x = v[i];
+    quicksort(vetor, 0, size - 1);
+    double ma = 0, mg = 1, dp, md, p95;
+    for (int i = 0; i < size; i++) {
+        double x = vetor[i];
         ma += x;
-        mg *= pow(x, 1.0 / n);
+        mg *= pow(x, 1.0 / size);
     }
-    ma /= n;
-    md = v[n / 2 - 1];
-    p95 = v[(95 * (n + 1)) / 100 - 1];
+    ma /= size;
+    md = vetor[size / 2 - 1];
+    p95 = vetor[(95 * (size + 1)) / 100 - 1];
     double aux_dp = 0.0;
-    for (int i = 0; i < n; i++) {
-        double aux_x = v[i] - ma;
-        aux_dp += (aux_x * aux_x) / n;
+    for (int i = 0; i < size; i++) {
+        double aux_x = vetor[i] - ma;
+        aux_dp += (aux_x * aux_x) / size;
     }
     dp = sqrt(aux_dp);
-
-    double end_time = omp_get_wtime();
+    maximo = vetor[size - 1];
+    minimo = vetor[0];
 
     FILE *filep;
-    char filename_out[1000];
-    strcpy(filename_out, "saidas/");
-    strcat(filename_out, filename);
-    filep = fopen(filename_out, "a");
-    fprintf(filep, "%s %.6lf %.6lf %.6lf %.6lf %.6lf %.6lf %.6lf\n", filename,
+    filep = fopen("saidas/results", "a");
+    fprintf(filep, "%s %.6lf %.6lf %.6lf %.6lf %.6lf %.6lf %.6lf\n", files[idx],
             ma, dp, mg, md, p95, minimo, maximo);
-    printf("%s - tempo: %.6lfs\n", filename, end_time - start_time);
+    fclose(filep);
+}
+
+int main(int argc, char *argv[]) {
+    int i, j;
+    int n;
+    n_threads = omp_get_max_threads();
+    omp_set_num_threads(n_threads);
+    double start_time_total = omp_get_wtime();
+    for (i = 1; i < argc; i++) {
+        n = open_file(i, argv);
+        double start_time = omp_get_wtime();
+        solve(i, argv, n);
+        double end_time = omp_get_wtime();
+        printf("%s - tempo: %.6lfs\n", argv[i], end_time - start_time);
+    }
+    double end_time_total = omp_get_wtime();
+    printf("Tempo total: %.6lfs\n", end_time_total - start_time_total);
+
     return 0;
 }
